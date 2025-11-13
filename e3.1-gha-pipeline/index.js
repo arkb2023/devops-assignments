854448eb04c772f6c17548f03e9fd0cb8c62b176
arkb2023 <ark.b2023@gmail.com>
2025-11-13 15:04:37 +0530

    e3.1-gha-pipeline: workflow test2

--- e3.1-gha-pipeline/index.js
+++ e3.1-gha-pipeline/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>[Test-1] Hello World!</h1>')
+  res.send('<h1>[Test-2] Hello World!</h1>')
 })
 
 const PORT = 8080
